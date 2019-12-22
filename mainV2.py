1bca4ac974bc40aa5f8780522e00103722078c5c
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2019-12-22 19:52:03 +0330

    what is even happening

--- mainV2.py
+++ mainV2.py
@@ -16,6 +16,43 @@ class MainApp(QMainWindow, ui):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.UI_Handler()
+        self.ButtonHandler()
+
+
+    ### UI_Handler ###
+    def UI_Handler(self):
+        self.hideThemeWindow
+
+    ### Button Handler ####
+    def ButtonHandler(self):
+        self.btnTheme.clicked.connect(self.showThemeWindow)
+        self.btnLightTheme.clicked.connect(self.setLightTheme)
+        self.btnDarkTheme.clicked.connect(self.setDarkTheme)
+        self.btnDarkOrangeTheme.clicked.connect(self.setDarkOrangeTheme)
+
+    ### Theme Window ###
+    def showThemeWindow():
+        self.ThemeWindow.setVisible(True)
+    
+    def hideThemeWindow():
+        self.ThemeWindow.setVisible(False)
+
+    def setLightTheme():
+        self.ThemeWindow.setVisible(False)
+
+    def setDarkTheme():
+        style = open('Themes\DarkGray.css', 'r')
+        style = style.read()
+        MainApp.setStyleSheet(style)
+        self.ThemeWindow.setVisible(False)
+    
+    def setDarkOrangeTheme():
+        style = open('Themes\DarkOrange.css', 'r')
+        style = style.read()
+        MainApp.setStyleSheet(style)
+        self.ThemeWindow.setVisible(False)
+
 
 
 def main():
