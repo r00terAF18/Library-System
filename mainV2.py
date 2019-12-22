db4f07cf05b67635babe419eeef1fa6be1a63437
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2019-12-22 17:46:43 +0330

    added initial setup code

--- mainV2.py
+++ mainV2.py
@@ -1,8 +1,25 @@
+import os
+import sqlite3
 import sys
+
+from PyQt5 import QtGui, uic
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUiType
-import sqlite3
+
 import icons_rc
 
+ui,_ = uic.loadUiType('Library System UI v2.ui')
+
+class MainApp(QMainWindow, ui):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
